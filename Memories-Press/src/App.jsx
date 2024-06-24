cbbde38d628f54afd7353dac79f5b23b3eb97c13
Elias Cleveland <eliascleveland1@outlook.com>
2024-06-24 19:16:26 +0800

    react router and basic layout

--- Memories-Press/src/App.jsx
+++ Memories-Press/src/App.jsx
@@ -1,39 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './layouts/Layout';
+import Homepage from './components/Homepage';
 
-const App = () => {
-    const [posts, setPosts] = useState([]);
-    const [error, setError] = useState(null);
-    const revPost = posts.reverse();
-    useEffect(() => {
-        fetch('http://memories-press/wp-json/wp/v2/posts')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => setPosts(data))
-            .catch(error => setError(error.toString()));
-    }, []);
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Layout />}>
+                <Route index element={<Homepage />}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>
+  )
+}
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-
-    return (
-        <div>
-            {posts.length > 0 ? (
-                posts.map(post => (
-                    <div key={post.id}>
-                        <h2>{post.title.rendered}</h2>
-                        <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
-                    </div>
-                ))
-            ) : (
-                <p>No posts available.</p>
-            )}
-        </div>
-    );
-};
-
-export default App;
+export default App
