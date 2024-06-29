d1f761c5432d47f53f6f42cc12c286a15663b885
Elias Cleveland <eliascleveland1@outlook.com>
2024-06-29 11:01:53 +0800

    mui install

--- Memories-Press/src/components/Header.jsx
+++ Memories-Press/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 function Header() {
   return (
-    <div>Header</div>
+    <NavBar />
   )
 }
 
-export default Header
+export default Header;
