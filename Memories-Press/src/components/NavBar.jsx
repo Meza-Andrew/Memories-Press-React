24efa950310272cdf5bd403d23f7ebf3e071507b
Elias Cleveland <eliascleveland1@outlook.com>
2024-07-30 16:57:40 +0800

    update info view adjustments

--- Memories-Press/src/components/NavBar.jsx
+++ Memories-Press/src/components/NavBar.jsx
@@ -51,184 +51,188 @@ function NavBar({isLoggedIn, setUser}) {
   
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <IconButton onClick={handleUserLogin}>
-            <LocalFloristIcon sx={{ display: { xs: 'flex', md: 'flex' }, mr: 1 }} />
-          </IconButton>
-          <Link component={RouterLink} to='/' color='inherit' underline='none'>
-          <Typography
-            variant="h6"
-            noWrap
-            sx={{
-              mr: 2,
-              flexGrow: 1,
-              display: { xs: 'flex', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Memories Press
-          </Typography>
-          </Link>
+    <>
+      <AppBar component="nav" position="fixed">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <IconButton onClick={handleUserLogin}>
+              <LocalFloristIcon sx={{ display: { xs: 'flex', md: 'flex' }, mr: 0 }} />
+            </IconButton>
+            <Link component={RouterLink} to='/' color='inherit' underline='none'>
+              <Typography
+                variant="h6"
+                noWrap
+                sx={{
+                  mr: 2,
+                  flexGrow: 1,
+                  display: { xs: 'flex', md: 'flex' },
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
+                Memories Press
+              </Typography>
+            </Link>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  component={RouterLink}
-                  to={`/${formatString(page)}`}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: 'white' }}
-                >
-                  {page}
-                </Button>
-              ))}
-              <Button variant="contained">Start your design</Button>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
+                {pages.map((page) => (
+                  <Button
+                    key={page}
+                    component={RouterLink}
+                    to={`/${formatString(page)}`}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white' }}
+                  >
+                    {page}
+                  </Button>
+                ))}
+                <Button variant="contained">Start your design</Button>
+              </Box>
             </Box>
-          </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' }, ml: 'auto' }}>
-            <IconButton
-              component={RouterLink}
-              to="/cart"
-              size="large"
-              aria-label="shopping cart"
-              color="inherit"
-            >
-              <ShoppingCartSharpIcon />
-            </IconButton>
-            {
-            isLoggedIn &&  
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            <Box sx={{ display: { xs: 'flex', md: 'none' }, ml: 'auto' }}>
+              <IconButton
+                component={RouterLink}
+                to="/cart"
+                size="large"
+                aria-label="shopping cart"
+                color="inherit"
+              >
+                <ShoppingCartSharpIcon />
               </IconButton>
-            </Tooltip>
-            }
-            <IconButton
-              size="large"
-              aria-label="menu"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {!isLoggedIn &&
-                <MenuItem 
-                  component={RouterLink}
-                  to={"/signin"}
-                  onClick={handleCloseNavMenu}
-                >
-                  <Typography textAlign="center">Log In</Typography>
-                </MenuItem>
+              {isLoggedIn &&  
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                  </IconButton>
+                </Tooltip>
               }
-              {!isLoggedIn && 
-                <MenuItem 
-                  component={RouterLink}
-                  to={"/signup"}
-                  onClick={handleCloseNavMenu}
-                >
-                  <Typography textAlign="center">Sign Up</Typography>
-                </MenuItem>
+              <IconButton
+                size="large"
+                aria-label="menu"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {!isLoggedIn &&
+                  <MenuItem 
+                    component={RouterLink}
+                    to={"/signin"}
+                    onClick={handleCloseNavMenu}
+                  >
+                    <Typography textAlign="center">Log In</Typography>
+                  </MenuItem>
+                }
+                {!isLoggedIn && 
+                  <MenuItem 
+                    component={RouterLink}
+                    to={"/signup"}
+                    onClick={handleCloseNavMenu}
+                  >
+                    <Typography textAlign="center">Sign Up</Typography>
+                  </MenuItem>
                 }
                 {!isLoggedIn && <Divider/>}
-              {pages.map((page) => (
-                <MenuItem 
-                  key={page} 
-                  component={RouterLink}
-                  to={`/${formatString(page)}`}
-                  onClick={handleCloseNavMenu}
-                >
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+                {pages.map((page) => (
+                  <MenuItem 
+                    key={page} 
+                    component={RouterLink}
+                    to={`/${formatString(page)}`}
+                    onClick={handleCloseNavMenu}
+                  >
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
 
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-          <IconButton
-              component={RouterLink}
-              to="/cart"
-              size="large"
-              aria-label="shopping cart"
-              color="inherit"
-            >
-              <ShoppingCartSharpIcon />
-            </IconButton>
-            {
-            isLoggedIn ? 
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Amy Sharp" src="/static/images/avatar/2.jpg" />
+            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+              <IconButton
+                component={RouterLink}
+                to="/cart"
+                size="large"
+                aria-label="shopping cart"
+                color="inherit"
+              >
+                <ShoppingCartSharpIcon />
               </IconButton>
-            </Tooltip>
-            :
-            <>
-              <Tooltip title="Login">
-                <Button component={RouterLink} sx={{ p: 0, color: 'white' }} to="/signin">
-                  <Typography>Login</Typography>
-                </Button>
-              </Tooltip>
-              <Tooltip title="Sign Up">
-                <Button component={RouterLink} sx={{ p: 0, color: 'white' }} to="/signup">
-                  <Typography>Sign Up</Typography>
-                </Button>
-              </Tooltip>
-            </>
-            }
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} component={RouterLink}
-                to={`/${formatString(setting)}`}
-                onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              {isLoggedIn ? 
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="Amy Sharp" src="/static/images/avatar/2.jpg" />
+                  </IconButton>
+                </Tooltip>
+              :
+                <>
+                  <Tooltip title="Login">
+                    <Button component={RouterLink} sx={{ p: 0, color: 'white' }} to="/signin">
+                      <Typography>Login</Typography>
+                    </Button>
+                  </Tooltip>
+                  <Tooltip title="Sign Up">
+                    <Button component={RouterLink} sx={{ p: 0, color: 'white' }} to="/signup">
+                      <Typography>Sign Up</Typography>
+                    </Button>
+                  </Tooltip>
+                </>
+              }
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem 
+                    key={setting} 
+                    component={RouterLink}
+                    to={`/${formatString(setting)}`}
+                    onClick={handleCloseUserMenu}
+                  >
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Toolbar /> {/* This Toolbar component adds padding to the main content below */}
+    </>
   );
 }
 
