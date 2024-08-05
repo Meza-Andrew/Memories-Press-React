7f848bddab7b452aaa09759a44067e1f74b09487
Elias Cleveland <eliascleveland1@outlook.com>
2024-08-05 14:04:39 +0800

    pathing adjustment two

--- Memories-Press/vite.config.js
+++ Memories-Press/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/memories-press/',
+  // base: 'https://demo.meza.design/memories-press/', // Try this one if the line above does not work.
   plugins: [react()],
 })
